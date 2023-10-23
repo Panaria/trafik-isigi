5dae2a296e3ffc18ad07c98380595da965cdcf40
Panaria <baranyldzby@gmail.com>
2023-10-23 19:45:38 +0300

    Add project files.

---
+++ trafik isigi/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace trafik_isigi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Location = new System.Drawing.Point(406, 54);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(175, 176);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DimGray;
+            this.panel2.Location = new System.Drawing.Point(406, 236);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(175, 176);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DimGray;
+            this.panel3.Location = new System.Drawing.Point(406, 418);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(175, 176);
+            this.panel3.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::trafik_isigi.Properties.Resources.png_clipart_traffic_light_road_t_truss_light_light_fixture_warning_sign;
+            this.ClientSize = new System.Drawing.Size(945, 649);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ trafik isigi/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trafik_isigi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.BackColor = Color.Red;
+            timer1.Start();
+        }
+        int panelNo = 1;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+          
+
+            if (panelNo == 1)
+            {
+                panel1.BackColor = Color.Red;
+                panel2.BackColor = Color.DimGray;
+                panel3.BackColor = Color.DimGray;
+                panelNo = 2;
+            }
+            else if (panelNo == 2)
+            {
+                panel1.BackColor = Color.DimGray;
+                panel2.BackColor = Color.Yellow;
+                panel3.BackColor = Color.DimGray;
+                panelNo = 3;
+            }
+            else if (panelNo == 3)
+            {
+                panel1.BackColor = Color.DimGray;
+                panel2.BackColor = Color.DimGray;
+                panel3.BackColor = Color.Green;
+                panelNo = 1;
+            }
+        }
+    }
+}
